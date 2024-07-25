4f29e8312ae26a8a512fd6ddb352159a02247482
RabbitezNoob <82694731+RabbitezNoob@users.noreply.github.com.>
2024-07-25 16:29:17 +0545

    added background worker to auditing nations

--- scripts/background_tasks.py
+++ scripts/background_tasks.py
@@ -6,18 +6,23 @@ import datetime as dt
 from googleapiclient.http import MediaFileUpload 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from discord.ext import commands
+from scripts.nation_data_converter import time_converter
 
 class background_tasks:
 
-	def __init__(self) -> None:
+	def __init__(self,bot) -> None:
+		self.channel = bot.get_channel(715222394318356541)
 		self.api_v3_link='https://api.politicsandwar.com/graphql?api_key=819fd85fdca0a686bfab'
 		self.update_nation_data.add_exception_type(OperationalError,KeyError,ReadTimeout,ConnectTimeout)
 		self.update_loot_data.add_exception_type(OperationalError,KeyError,ReadTimeout,ConnectTimeout)
 		self.update_trade_price.add_exception_type(OperationalError,KeyError,ReadTimeout,ConnectTimeout)
+		self.audit_members.add_exception_type(OperationalError,KeyError,ReadTimeout,ConnectTimeout)
 		self.update_trade_price.start()
 		self.update_nation_data.start()
 		self.update_loot_data.start()
 		self.copy_db.start()
+		self.audit_members.start()
 		pass
 
 	@tasks.loop(minutes=5,reconnect=True)
@@ -197,4 +202,57 @@ class background_tasks:
 		logging.info(f'File ID: {file.get("id")}')		
 		drive_service.close()
 
+	@tasks.loop(hours=24,reconnect=True)
+	async def audit_members(self):
+		query="""{alliances(id:11189){
+  	data{
+    	color
+   	 	nations{
+			last_active,color,id,alliance_position,soldiers,tanks,aircraft,ships,spies,num_cities,discord,discord_id}
+    	}
+		}
+	}"""
+		async with httpx.AsyncClient() as client:
+			fetchdata=await client.post(self.api_v3_link,json={'query':query})
+			fetchdata = fetchdata.json()["data"]["alliances"]["data"]
+		mmr = [0 * 3000 ,2 * 250,5 * 15, 0 * 5]	
+		unit_name = ["soldiers","tanks","aircraft","ships"]
+		for nation in fetchdata["nations"]:
+			if nation["alliance_position"]!="APPLICANT":
+				if nation["color"]!=fetchdata["color"]:
+					text = await self.member_info(nation)
+					await self.channel.send(f"{text} please change your color {fetchdata["color"]}")
+				inactive_days = time_converter(dt.strptime(nation["last_active"],'%Y-%m-%d %H:%M:%S')).split('d')[0]
+				if inactive_days>5:
+					text = await self.member_info(nation)
+					await self.channel.send(f"{text} please login you have been inactive for {inactive_days} day(s).")
+				mmr_nation = [x * nation["num_cities"] for x in mmr]
+				mmr_violation = False
+				violation_text = ""
+				for units in range(0,mmr):
+					if nation[unit_name[units]]<mmr_nation[units]:
+						mmr_violation = True
+						violation_text = f"{text} You are missing {mmr_nation[units]-nation[unit_name[units]]} {unit_name[units]} to reach the mmr.\n"
+				if mmr_violation:
+					text = await self.member_info(nation)
+					await self.channel.send(f"{text} {violation_text}")		
+		logging.info("Task audit_members ran successfully")
 
+	async def member_info(self,nation):
+		discord_id = None
+		if nation["discord_id"]!=None:
+			discord_id = f"<@{nation["discord_id"]}>"
+		elif nation["discord"]!="":
+			discord_id= await commands.converter.MemberConverter().convert(self.channel,nation["discord"])
+			discord_id = f"<@{discord_id}"
+		else:
+			async with aiosqlite.connect('pnw.db') as db:	
+				async with db.execute(f'select discord_id from registered_nations where nation_id={nation["id"]}') as cursor:
+					discord_id = await cursor.fetchone()
+			if discord_id!=None:
+				discord_id = f"<@{discord_id[0]}>"			
+		if discord_id!=None:
+			nation["discord_id"]=discord_id
+			return discord_id
+		else:
+			return f"https://policticsandwar.com/nation/id={nation["id"]} is not registered to the bot\n"
