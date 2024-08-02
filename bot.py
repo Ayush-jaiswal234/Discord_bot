b3d022296ae97f14536dc493ca5a6d0903135865
RabbitezNoob <82694731+RabbitezNoob@users.noreply.github.com.>
2024-08-02 18:44:27 +0545

    1024 character limit error fix for wars

--- bot.py
+++ bot.py
@@ -449,12 +449,12 @@ async def wars(ctx,*,_id=None):
 		if int(war['att_id'])==nation_id:
 			off_text=(f"{off_text}{count_off}. [{await nation_data_converter.get_unregistered('nation',war['def_id'])}](https://politicsandwar.com/nation/id={war['def_id']})\t"
 		 			f"`AR:{war['att_resistance']}` `DR:{war['def_resistance']}`\n"
-					f"Alliance: [{await nation_data_converter.get_unregistered('alliance',war['def_id'])}](https://politicsandwar.com/nation/id={war['def_alliance_id']})\n")
+					f"Alliance: {await nation_data_converter.get_unregistered('alliance',war['def_id'])}\n")
 			count_off+=1
 		else:
 			def_text=(f"{def_text}{count_def}. [{await nation_data_converter.get_unregistered('nation',war['att_id'])}](https://politicsandwar.com/nation/id={war['att_id']})\t"
 		 			f"`AR:{war['att_resistance']}` `DR:{war['def_resistance']}`\n"
-					f"Alliance: [{await nation_data_converter.get_unregistered('alliance',war['att_id'])}](https://politicsandwar.com/nation/id={war['att_alliance_id']})\n")
+					f"Alliance: {await nation_data_converter.get_unregistered('alliance',war['att_id'])}\n")
 			count_def+=1
 		
 	if off_text!="":
